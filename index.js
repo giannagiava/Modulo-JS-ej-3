cd7b202573ad2db291971640793adbfb74c8c3da
Gianna Giavarini <giannagiavarini@outlook.com>
2021-07-02 18:56:38 -0200

    ejercitacion js

---
+++ index.js
@@ -0,0 +1,8 @@
+let nombre = prompt ("Ingrese su nombre")
+let apellido = prompt ("Ingrese su apellido")
+let nacionalidad = prompt ("Ingrese su nacionalidad")
+let documento = prompt ("Ingrese su documento")
+
+console.log (nombre + " " + apellido + " "  + nacionalidad + " " + documento)
+
+prompt ("Nuevo usuario agregado al sistema: " + nombre + " " + apellido + " "  + nacionalidad + " " + documento )
